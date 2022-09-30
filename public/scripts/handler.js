2a5f0618a18786ed59f35e0a9d7cb29a9e3596e7
Shuai Hu <shuiandy@gmail.com>
2022-09-29 21:06:27 -0400

    disabled some functions

--- public/scripts/handler.js
+++ public/scripts/handler.js
@@ -1,20 +1,20 @@
-$(document).ready(function () {
-  $('#inputItemName').on('submit', function (event) {
-    // event.preventDefault();
-    const itemName = $('#inputItemName').val().trim();
-    $.ajax({
-      type: 'post',
-      url: '/api/new-item',
-      data: {  data},
-    }).done(function (data) {
-      if (data.hasDuplicates) {
-        alert('Do not duplicate items!');
-      }
-      if (data.redirect) {
-        window.location.replace('/index');
-      }
-    });
-  });
+// $(document).ready(function () {
+//   $('#inputItemName').on('submit', function (event) {
+//     // event.preventDefault();
+//     const itemName = $('#inputItemName').val().trim();
+//     $.ajax({
+//       type: 'post',
+//       url: '/api/new-item',
+//       data: {  data},
+//     }).done(function (data) {
+//       if (data.hasDuplicates) {
+//         alert('Do not duplicate items!');
+//       }
+//       if (data.redirect) {
+//         window.location.replace('/index');
+//       }
+//     });
+//   });
 
   $('#delete-item').click(function () {
     const itemName = $('#detail-item-name').text().trim();
